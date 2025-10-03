684aba5ae28780c6c30e540260947c403d556378
gfranice <gfranicevich@mi.unc.edu.ar>
2025-10-03 15:21:13 -0300

    waw

--- gabi.sql
+++ gabi.sql
@@ -31,14 +31,38 @@ ORDER BY `ventas totales` desc;
 -- 4. Crear una vista que liste los empleados con más ventas por cada año, mostrando
 -- empleado, año y total de ventas. Ordenar el resultado por año ascendente.
 
--- employees -> orders
+-- employees -> orders -> order details
+SELECT e.EmployeeID, e.LastName, e.FirstName , SUM(od.Quantity * od.UnitPrice - od.Discount ) AS ventas, YEAR(o.OrderDate) as year
+FROM employees e 
+INNER JOIN orders o ON e.EmployeeID = o.EmployeeID
+INNER JOIN `order details` od  ON o.OrderID = od.OrderID
+GROUP BY e.EmployeeID, year
+ORDER BY year, ventas DESC;
+
 
 
 -- 5. Crear un trigger que se ejecute después de insertar un nuevo registro en la tabla
 -- Order Details. Este trigger debe actualizar la tabla Products para disminuir la
 -- cantidad en stock (UnitsInStock) del producto correspondiente, restando la
 -- cantidad (Quantity) que se acaba de insertar en el detalle del pedido.
---
+
+delimiter $$
+
+CREATE TRIGGER new_order
+AFTER INSERT ON `order details`
+FOR EACH ROW
+BEGIN
+	DECLARE product INT;
+
+	SELECT od.ProductID  INTO product
+	FROM `order details` od 
+	WHERE od.ProductID = new.ProductID;
+
+	UPDATE products SET UnitsInStock = UnitsInStock - new.Quantity;
+END$$
+
+
+delimiter ;
 
 -- 6. Crear un rol llamado admin y otorgarle los siguientes permisos:
 --  crear registros en la tabla Customers.
