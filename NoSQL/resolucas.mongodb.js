376b6b061ba3a66240ca5c11c0d78d0d7b75d1f7
Lucas Sabino <sabino.lucas2016@gmail.com>
2025-10-31 14:54:16 -0300

    .

--- NoSQL/resolucas.mongodb.js
+++ NoSQL/resolucas.mongodb.js
@@ -5,7 +5,7 @@ db.listingsAndReviews.aggregate([
     $group: {
       _id: "$address.country",
       average_rating: { $avg: "$review_scores.review_scores_rating" },
-      total_count: {$count: {}} // Total rating
+      total_count: {$count: {}}
     }
   },
   {
@@ -25,9 +25,28 @@ db.listingsAndReviews.aggregate([
 
 /* Ejercicio 2 */
 
-/** 
- * 2. Listar los 20 alojamientos que tienen las reviews más recientes. Listar el id, nombre,
- * fecha de la última review, y cantidad de reviews del alojamiento. Listar en orden
- * descendente por cantidad de reviews.
- * HINT: $first pueden ser de utilidad.
- */
+db.listingsAndReviews.aggregate([
+  {
+    $match: {
+      "last_review": { $ne: null, $exists: true }
+    }
+  },
+  {
+    $sort: { "last_review": -1 }
+  },
+  {
+    $limit: 20
+  }, 
+  {
+    $project: {
+      name: 1,
+      last_review: 1,
+      number_of_reviews: 1
+    }
+  },
+  {
+    $sort: { number_of_reviews: -1 }
+  }
+]);
+
+/* Ejercicio 3 */
