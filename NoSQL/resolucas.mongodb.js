d94c24cae61be24c04623d736aa359d11c127d31
Lucas Sabino <sabino.lucas2016@gmail.com>
2025-10-31 15:23:55 -0300

    .

--- NoSQL/resolucas.mongodb.js
+++ NoSQL/resolucas.mongodb.js
@@ -1,5 +1,4 @@
 /* Ejercicio 1 */
-
 db.listingsAndReviews.aggregate([
   {
     $group: {
@@ -23,8 +22,11 @@ db.listingsAndReviews.aggregate([
   }
 ]);
 
-/* Ejercicio 2 */
 
+db.reviews.aggregate([{$group: {_id: "$address.country",average_rating: { $avg: "$review_scores.review_scores_rating" },total_count: {$count: {}}}},{$project: {country: "$_id",average_rating: 1,total_count: 1,_id: 0}},{$sort:{average_rating: -1}}]);
+
+
+/* Ejercicio 2 */
 db.listingsAndReviews.aggregate([
   {
     $match: {
@@ -49,8 +51,9 @@ db.listingsAndReviews.aggregate([
   }
 ]);
 
-/* Ejercicio 3 */
+db.reviews.aggregate([{$match: {"last_review": { $ne: null, $exists: true }}},{$sort: { "last_review": -1 }},{$limit: 20}, {$project: {name: 1,last_review: 1,number_of_reviews: 1}},{$sort: { number_of_reviews: -1 }}]);
 
+/* Ejercicio 3 */
 db.createView(
   "top10_most_common_amenities",
   "listingAndReviews",
@@ -72,3 +75,41 @@ db.createView(
     }
   ]
 );
+
+/* Ejercicio 4 */
+
+/* Actualizar los alojamientos de Brazil que tengan un rating global
+(“review_scores.review_scores_rating”) asignado, agregando el campo
+"quality_label" que clasifique el alojamiento como “High” (si el rating global es mayor
+o igual a 90), “Medium” (si el rating global es mayor o igual a 70), “Low” (valor por
+defecto) calidad..
+HINTS: (i) para actualizar se puede usar pipeline de agregación. (ii) El operador
+$cond o $switch pueden ser de utilidad */
+
+db.listingsAndReviews.updateMany(
+   {
+      "address.country": "Brazil",
+      "review_scores.review_scores_rating": { $exists: true, $ne: null }
+   },
+   [
+      {
+         $set: {
+            "quality_label": {
+               $switch: {
+                  branches: [
+                     {
+                        case: { $gte: ["$review_scores.review_scores_rating", 90] },
+                        then: "High"
+                     },
+                     {
+                        case: { $gte: ["$review_scores.review_scores_rating", 70] },
+                        then: "Medium"
+                     }
+                  ],
+                  default: "Low"
+               }
+            }
+         }
+      }
+   ]
+)
