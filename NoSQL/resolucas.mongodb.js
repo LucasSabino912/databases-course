d2856f48b4ef7d9bcc509f8002048cbb3e36b8c3
Lucas Sabino <sabino.lucas2016@gmail.com>
2025-10-31 15:53:52 -0300

    Ejercicio 5 y 4 funcionan

--- NoSQL/resolucas.mongodb.js
+++ NoSQL/resolucas.mongodb.js
@@ -22,10 +22,6 @@ db.listingsAndReviews.aggregate([
   }
 ]);
 
-
-db.reviews.aggregate([{$group: {_id: "$address.country",average_rating: { $avg: "$review_scores.review_scores_rating" },total_count: {$count: {}}}},{$project: {country: "$_id",average_rating: 1,total_count: 1,_id: 0}},{$sort:{average_rating: -1}}]);
-
-
 /* Ejercicio 2 */
 db.listingsAndReviews.aggregate([
   {
@@ -51,8 +47,6 @@ db.listingsAndReviews.aggregate([
   }
 ]);
 
-db.reviews.aggregate([{$match: {"last_review": { $ne: null, $exists: true }}},{$sort: { "last_review": -1 }},{$limit: 20}, {$project: {name: 1,last_review: 1,number_of_reviews: 1}},{$sort: { number_of_reviews: -1 }}]);
-
 /* Ejercicio 3 */
 db.createView(
   "top10_most_common_amenities",
@@ -77,15 +71,6 @@ db.createView(
 );
 
 /* Ejercicio 4 */
-
-/* Actualizar los alojamientos de Brazil que tengan un rating global
-(“review_scores.review_scores_rating”) asignado, agregando el campo
-"quality_label" que clasifique el alojamiento como “High” (si el rating global es mayor
-o igual a 90), “Medium” (si el rating global es mayor o igual a 70), “Low” (valor por
-defecto) calidad..
-HINTS: (i) para actualizar se puede usar pipeline de agregación. (ii) El operador
-$cond o $switch pueden ser de utilidad */
-
 db.listingsAndReviews.updateMany(
   {
     "address.country": "Brazil",
@@ -113,3 +98,105 @@ db.listingsAndReviews.updateMany(
     }
   ]
 )
+
+/* Ejercicio 5 */
+db.runCommand({
+  collMod: "listingsAndReviews",
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["name", "address", "amenities", "review_scores", "reviews"], // campos obligatorios
+      properties: {
+        name: {
+          bsonType: "string",
+          description: "El campo 'name' debe ser un string."
+        },
+        address: {
+          bsonType: "object",
+          required: ["country"],
+          properties: {
+            country: {
+              bsonType: "string",
+              description: "El campo 'address.country' debe ser un string."
+            }
+          }
+        },
+        amenities: {
+          bsonType: "array",
+          items: {
+            bsonType: "string"
+          },
+          description: "El campo 'amenities' debe ser un array de strings."
+        },
+        review_scores: {
+          bsonType: "object",
+          required: ["review_scores_rating"],
+          properties: {
+            review_scores_rating: {
+              bsonType: ["int", "double"],
+              description: "El campo 'review_scores_rating' debe ser int o double."
+            }
+          }
+        },
+        reviews: {
+          bsonType: "array",
+          items: {
+            bsonType: "object"
+          },
+          description: "El campo 'reviews' debe ser un array de objetos."
+        }
+      }
+    }
+  },
+  validationAction: "error",
+  validationLevel: "strict"
+});
+
+
+/* Documento valido */
+db.listingsAndReviews.insertOne({
+  name: "Apartamento de Prueba Válido",
+  address: {
+    street: "Calle Válida 123",
+    country: "Argentina"
+  },
+  amenities: ["Wifi", "Cocina", "Aire acondicionado"],
+  review_scores: {
+    review_scores_rating: 95.5,
+    review_scores_cleanliness: 10
+  },
+  reviews: [
+    { _id: "rev1", comments: "Excelente estadía." }
+  ],
+  listing_url: "http://test.com/valid"
+});
+
+
+/* Documento invalido falta amenitie */
+db.listingsAndReviews.insertOne({
+  name: "Fallo 1",
+  address: {
+    country: "Chile"
+  },
+  review_scores: {
+    review_scores_rating: 88
+  },
+  reviews: [
+    { _id: "rev2", comments: "Bien." }
+  ]
+});
+
+/*  Documento invalido */
+db.listingsAndReviews.insertOne({
+  name: "Fallo 2",
+  address: {
+    country: 123456
+  },
+  amenities: ["Wifi"],
+  review_scores: {
+    review_scores_rating: "Muy Bueno"
+  },
+  reviews: [
+    { _id: "rev3", comments: "Regular." }
+  ]
+});
